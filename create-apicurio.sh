62450030f4d4aeb66644a27faf385c93db0ca5eb
Evrim <evrim@tr.ibm.com>
2019-10-25 14:04:30 +0300

    fixed microcks template issues

--- create-apicurio.sh
+++ create-apicurio.sh
@@ -1,7 +1,7 @@
 oc new-app -f apicurio-standalone-template.yml \
-  -p UI_ROUTE=apicurio-studio.192.168.42.25.nip.io \
-  -p API_ROUTE=apicurio-studio-api.192.168.42.25.nip.io \
-  -p WS_ROUTE=apicurio-studio-ws.192.168.42.25.nip.io \
-  -p AUTH_ROUTE=apicurio-studio-auth.192.168.42.25.nip.io \
+  -p UI_ROUTE=apicurio-studio.192.168.99.100.nip.io \
+  -p API_ROUTE=apicurio-studio-api.192.168.99.100.nip.io \
+  -p WS_ROUTE=apicurio-studio-ws.192.168.99.100.nip.io \
+  -p AUTH_ROUTE=apicurio-studio-auth.192.168.99.100.nip.io \
   -p GENERATED_KC_USER=admin \
   -p GENERATED_KC_PASS=admin
--- create-microcks.sh
+++ create-microcks.sh
@@ -1,7 +1,9 @@
 oc new-app -f microcks-openshift-persistent-full-template.yml \
-    -p OPENSHIFT_MASTER=https://192.168.42.25:8443 \
+    -p OPENSHIFT_MASTER=https://192.168.99.100:8443 \
     -p OPENSHIFT_OAUTH_CLIENT_NAME=microcks \
-    -p APP_ROUTE_HOSTNAME=microcks.192.168.42.25.nip.io \
-    -p KEYCLOAK_ROUTE_HOSTNAME=microcks-auth.192.168.42.25.nip.io 
+    -p APP_ROUTE_HOSTNAME=microcks.192.168.99.100.nip.io \
+    -p KEYCLOAK_ROUTE_HOSTNAME=microcks-auth.192.168.99.100.nip.io \
+    -p KEYCLOAK_ADMIN_USERNAME=admin \
+    -p KEYCLOAK_ADMIN_PASSWORD=admin 
     
 
